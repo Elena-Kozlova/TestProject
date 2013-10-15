10c928ae38449c02596ecb3abb882046c87c09bf
Kozlova Elena <elena.kozlova@oxagile.com>
2013-10-15 14:24:21 +0300

    add tabs js + carousel

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var tabs = [];
+var tabHeaders = [];
+
+$(document).ready(function () {
+    initCarousel();
+
+    initTabs();
+});
+
+function initCarousel() {
+    $('#liquid1').liquidcarousel({height:129, duration:100, hidearrows:false});
+}
+
+function initTabs() {
+    tabs.push($("#tabContent1"));
+    tabs.push($("#tabContent2"));
+    tabs.push($("#tabContent3"));
+
+    tabHeaders.push($("#tabHeader1"));
+    tabHeaders.push($("#tabHeader2"));
+    tabHeaders.push($("#tabHeader3"));
+
+    activateTab(0);
+
+    $("#tabLink1").bind("click", function () {
+        activateTab(0);
+    });
+
+    $("#tabLink2").bind("click", function () {
+        activateTab(1);
+    });
+
+    $("#tabLink3").bind("click", function () {
+        activateTab(2);
+    });
+}
+
+function activateTab(tabNum) {
+    for (var i = 0; i < 3; i++) {
+        if (i == tabNum) {
+            tabs[i].show();
+            tabHeaders[i].addClass("active");
+        } else {
+            tabs[i].hide();
+            tabHeaders[i].removeClass("active");
+        }
+    }
+}
